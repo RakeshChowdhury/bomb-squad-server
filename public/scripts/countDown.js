0e82b02eedae3e8155bac65ee603900c99a89fc8
yashdeep97 <yashdeep97@gmail.com>
2017-09-02 02:46:30 +0530

    js: added functionality to buttons

--- public/scripts/countDown.js
+++ public/scripts/countDown.js
@@ -2,18 +2,32 @@
 var time = 30;
 var timeleft = time;
 var status = 0;
-var x = setInterval(function() {
 
 
+var x = setInterval(function(){ progress() }, 1000);
+
+
+function progress() {
     timeleft = timeleft - 1;
     status = ((time - timeleft)/time)*100;
 
     document.getElementById("countdown").innerHTML = timeleft + "s";
     document.getElementById("progressbar").style.width = status.toString() + "%";
-    
-    // If the count down is over, write some text 
+
     if (timeleft < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPLODED";
     }
-}, 1000);
+}
+
+function explode(){
+	clearInterval(x);
+	timeleft = -1;
+	document.getElementById("countdown").innerHTML = "EXPLODED";
+	document.getElementById("progressbar").style.width = "100%";
+}
+
+function defuse(){
+	clearInterval(x);
+	document.getElementById("countdown").innerHTML = "DEFUSED";
+}
