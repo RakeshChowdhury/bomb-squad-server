4dfd1d11033c9f17aab83c3aafeaffff1d6f5851
arnav dhamija <arnav.dhamija@gmail.com>
2017-09-12 20:17:04 +0530

    Added a new class

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 var express = require('express');
-
-
-
 var app = express();
-
-app.set('view engine', 'pug');
-app.set('views','./views');
-
-app.get('/', function(req, res){
-   res.render('countDown');
-});
-
-
+var Bomb = require('./bomb.js');
 var key = String(process.argv.slice(2));
-app.get('/' + key + '/:rfid([0-9]{12})', function(req, res){
-	console.log(req.params.rfid);
-});
-
 
 app.use('/js', express.static(__dirname + '/node_modules/mdbootstrap/js/'));
 app.use('/css', express.static(__dirname + '/node_modules/mdbootstrap/css/'));
 app.use('/css', express.static(__dirname + '/public/stylesheets/'));
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+var states = {
+    ARMED: 0,
+    DISARMED: 1,
+    DEFUSED: 2,
+    EXPLODED: 3
+};
+
+var inst = new Bomb(8,1238912,states.EXPLODED);
+console.log(inst.status);
+
+app.get('/', function(req, res){
+   res.render('countDown');
+});
+
+app.get('/' + key + '/:rfid([0-9]{12})', function(req, res){
+	console.log(req.params.rfid);
+});
 
 app.listen(8085);
